74041950831fd1119d2a33e3eaf25e723b56e3b7
Rofer11607 <rafac@monsterrg.com>
2023-02-20 08:12:55 -0500

    testing safety

--- libs/nrwl-blaze/src/lib/code-paths/delete/delete.ts
+++ libs/nrwl-blaze/src/lib/code-paths/delete/delete.ts
@@ -1,30 +1,40 @@
-import {prompt} from 'inquirer'
-import {Config} from "../../shared/config/config";
-import {runCommand} from '../../shared/run-command/run-command';
+import { prompt } from 'inquirer';
+import { Config } from '../../shared/config/config';
+import { runCommand } from '../../shared/run-command/run-command';
 
 export async function deleteProject() {
   const config = new Config();
-  const apps = config.apps.allProjects.filtered
-  const libs = config.libs.allProjects.filtered
-  const appsAndLibs = apps.concat(libs)
+  const apps = config.apps.allProjects.filtered;
+  const libs = config.libs.allProjects.filtered;
+  const appsAndLibs = apps.concat(libs);
+  const rawNames = [
+    ...config.apps.allProjects.raw,
+    ...config.libs.allProjects.raw,
+  ].map((p) => p.split('/').pop());
 
-  const {projects} = await prompt([
+  const { projects } = await prompt([
     {
       type: 'checkbox',
       name: 'projects',
       message: 'Which projects would you like to delete?',
       choices: appsAndLibs,
-    }
-  ])
+    },
+  ]);
 
-  for(const project of projects) {
-    const name = project.split('/').pop()
-    if(name==='nrwl-blaze') return
-    const e2eName =`${name}-e2e`
-    if(appsAndLibs.includes(e2eName)) {
-      await runCommand(`nx g @nrwl/workspace:remove ${e2eName}`)
+  for (const project of projects) {
+    const name = project.split('/').pop();
+    if (name === 'nrwl-blaze') {
+      console.log('cant delete nrwl-blaze, its too good')
+      return
     }
-    await runCommand(`nx g @nrwl/workspace:remove ${name}`)
-    config.blazeConfig.removeFromBlazeConfig(name)
+    const e2eName = `${name}-e2e`;
+
+    console.log({ e2eName, rawNames, project });
+    if (rawNames.includes(e2eName)) {
+      await runCommand(`npx nx g @nrwl/workspace:remove ${e2eName}`);
+    }
+
+    await runCommand(`npx nx g @nrwl/workspace:remove ${name}`);
+    config.blazeConfig.removeFromBlazeConfig(name);
   }
 }
--- libs/nrwl-blaze/src/lib/code-paths/index.ts
+++ libs/nrwl-blaze/src/lib/code-paths/index.ts
@@ -1,3 +1,4 @@
 export * from './create/create'
 export * from './register/register'
 export * from './deploy/deploy'
+export * from './delete/delete'
